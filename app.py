66b9c281518d43ae10ec4307afa4547887da2b8e
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2023-05-13 20:12:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def predict():
 
     result = model.predict(input_query)[0]
 
-    return jsonify({'placement':str(result)})
+    return jsonify({'addicted':str(result)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=8080)
