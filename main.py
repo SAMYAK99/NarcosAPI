d380ae540c6a4aadfbc235fe7ad8128c993ec96e
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2023-05-13 20:44:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,4 +39,4 @@ def predict():
     return jsonify({'addicted':str(result)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host="0.0.0.0", port=5000, debug=True)
